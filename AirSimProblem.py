bce69aa81e62725e858ca09be7cdfbaf38157186
itzik1058 <74200683+itzik1058@users.noreply.github.com>
2021-07-27 19:53:17 +0300

    A*

--- AirSimProblem.py
+++ AirSimProblem.py
@@ -2,10 +2,12 @@ import numpy as np
 from search import Problem
 from collections import defaultdict
 from itertools import permutations
+from utils import hashabledict
+import random
 
 
 class airsimproblem(Problem):
-    def __init__(self, num_drones, max_dist, initial, goal, packages_locations):
+    def __init__(self, max_dist, initial, goal, packages_locations):
         super(airsimproblem, self).__init__(initial, goal)
         self.num_drones = len(initial['drones'])
         self.max_dist = max_dist
@@ -24,10 +26,11 @@ class airsimproblem(Problem):
         1. for each drone -> if at home & have room for package: check packages -> if reachable: lift go to dest, else: stay.
         2. for each drone -> if at destination & have package: go to next destination, else: go home.
         """
-        legal_actions = {}
         drone_package_pairs = defaultdict(list)
         free_packages = []
-        avilable_drones = []
+        available_drones = []
+        unavailable_drones = []
+        home = []
         for package, drone in enumerate(state['packages']):
             if drone > 0:
                 drone_package_pairs[drone].append(package)
@@ -36,10 +39,50 @@ class airsimproblem(Problem):
 
         for drone, location in enumerate(state['drones']):
             if location == 0 and len(drone_package_pairs[drone]) < 2:
-                avilable_drones.append(drone)
-            elif len(drone_package_pairs[drone]) == 2:
-                legal_actions[f'drone_{drone}'] = -1
+                available_drones.append(drone)
+            elif location != 0 and len(drone_package_pairs[drone]) == 0:
+                home.append(drone)
+            else:
+                unavailable_drones.append(drone)
+        actions = []
+        for perm in permutations(free_packages, min(len(available_drones), len(free_packages))):
+            action = [0] * len(state['drones'])
+            for i, drone in enumerate(available_drones):
+                if i >= len(free_packages):
+                    break
+                action[drone] = -perm[i]  # -x    Pickup package x
+            for drone in unavailable_drones:
+                action[drone] = drone_package_pairs[drone][0]  # x     Go to location of package x
+            for drone in home:
+                action[drone] = 0  # 0     Go to home
+            actions.append(tuple(action))
+        print(actions)
+        if len(actions) > 20:
+            return random.sample(actions, 20)
+        if len(actions) == 0:
+            x = 5
+        return actions
 
+    def result(self, state, action):
+        print(action)
+        print([location for location in state['packages']])
+        new_state = hashabledict()
+        new_state['drones'] = list(state['drones'])
+        new_state['packages'] = list(state['packages'])
+        for drone, operation in enumerate(action):
+            if operation >= 0:
+                new_state['drones'][drone] = operation
+                new_state['packages'][operation] = -1
+            else:
+                new_state['packages'][operation] = drone
+        new_state['drones'] = tuple(new_state['drones'])
+        new_state['packages'] = tuple(new_state['packages'])
+        print(new_state)
+        return new_state
+
+    def h(self, node):
+        state = node.state
+        return -100 * len([0 for location in state['packages'] if location == -1])
 
     # def TakeOff(self):
     #     for drone in range(self.num_drones):
@@ -49,5 +92,3 @@ class airsimproblem(Problem):
     # def Land(self):
     #     for drone in range(self.num_drones):
     #         self.client.landAsync(vehicle_name=f'drone_{drone}').join()
-
-
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # ready to run example: PythonClient/multirotor/hello_drone.py
 import airsim
+
+import search
 from AirSimProblem import airsimproblem
 from utils import hashabledict
 import numpy as np
@@ -9,42 +11,43 @@ num_drones = 5
 num_packages = 20
 max_dist = 1e+6
 initial = hashabledict()
-initial['drones'] = [0 for _ in range(num_drones)]
-initial['packages'] = [0 for _ in range(num_packages)]
+initial['drones'] = tuple(0 for _ in range(num_drones))
+initial['packages'] = tuple(0 for _ in range(num_packages))
 
 goal = hashabledict()
-goal['drones'] = [0 for _ in range(num_drones)]
-goal['packages'] = [-1 for _ in range(num_packages)]
+goal['drones'] = tuple(0 for _ in range(num_drones))
+goal['packages'] = tuple(-1 for _ in range(num_packages))
 
 packages_locations = np.random.randint(-100, 100, size=(num_packages, 2))  # generating 20 coordinates, ignoring z - assuming on land
 
 asprob = airsimproblem(max_dist, initial, goal, packages_locations)
+print(search.astar_search(asprob))
 
 
 
 
-# connect to the AirSim simulator
-client = airsim.MultirotorClient()
-client.confirmConnection()
-client.enableApiControl(True)
-client.armDisarm(True)
-
-# Async methods returns Future. Call join() to wait for task to complete.
-client.takeoffAsync().join()
-client.moveToPositionAsync(-20, 17, -50, 5).join()
-client.landAsync().join()
-client.getHomeGeoPoint() #lat,long,altitude,time
-client.getGpsData() #velocity: xyz + lat,long,altitude,time
-client.getMultirotorState() # gps data + kinematics + position
-client.goHomeAsync()
-
-
-# add new vehicle
-vehicle_name = "Drone2"
-pose = airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(0, 0, 0))
-
-client.simAddVehicle(vehicle_name, "simpleflight", pose)
-client.enableApiControl(True, vehicle_name)
-client.armDisarm(True, vehicle_name)
-client.takeoffAsync(10.0, vehicle_name)
+# # connect to the AirSim simulator
+# client = airsim.MultirotorClient()
+# client.confirmConnection()
+# client.enableApiControl(True)
+# client.armDisarm(True)
+#
+# # Async methods returns Future. Call join() to wait for task to complete.
+# client.takeoffAsync().join()
+# client.moveToPositionAsync(-20, 17, -50, 5).join()
+# client.landAsync().join()
+# client.getHomeGeoPoint() #lat,long,altitude,time
+# client.getGpsData() #velocity: xyz + lat,long,altitude,time
+# client.getMultirotorState() # gps data + kinematics + position
+# client.goHomeAsync()
+#
+#
+# # add new vehicle
+# vehicle_name = "Drone2"
+# pose = airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(0, 0, 0))
+#
+# client.simAddVehicle(vehicle_name, "simpleflight", pose)
+# client.enableApiControl(True, vehicle_name)
+# client.armDisarm(True, vehicle_name)
+# client.takeoffAsync(10.0, vehicle_name)
 
